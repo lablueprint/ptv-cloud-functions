8d6472b899dc2c8faad79ca24f0d7ca6988c8807
Bill Liu <bill.liu.school@gmail.com>
2020-05-15 23:37:27 -0400

    Added update to admin auth upon ban event

--- functions/firestore/users/onUpdate.f.js
+++ functions/firestore/users/onUpdate.f.js
@@ -1,10 +1,16 @@
 import { firestore } from 'firebase-functions';
-import admin from 'firebase-admin';
+
+const admin = require('firebase-admin');
 
 export default firestore
   .document('users/{userId}')
   .onUpdate((change, context) => {
     const { userId } = context.params;
+    const currBody = change.after.data();
+
+    admin.auth().updateUser(userId, {
+      disabled: currBody.isBanned,
+    });
 
     return null;
   });
